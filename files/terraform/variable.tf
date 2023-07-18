4cb401b7548e298695756ab52b595d1429fbc776
Alexey Pajitnov <117950191+alexpajitnov111@users.noreply.github.com>
2023-07-18 08:10:41 +0300

    111

---
+++ files/terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "YC_TOKEN" {
+  default = ""
+}
+
+variable "YC_CLOUD_ID" {
+  default = ""
+}
+
+
+variable "YC_FOLDER_ID" {
+  default = ""
+}
+
+variable "YC_ZONE" {
+  default = "ru-central1-a"
+}
